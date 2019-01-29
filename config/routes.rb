a2b9d1213c54c9b9fd90971d0c84e584d7747022
André de Souza Lima <andre.szlima1@gmail.com>
2019-01-28 21:41:24 -0300

    Controllers
    Completed all controllers/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,56 @@
 Rails.application.routes.draw do
+
+  root 'books#index'
+
+  resources :books
+  resources :authors
+  resources :categories
+  resources :publishers
+
+  get 'about' => 'about#index'
+
+  get 'publishers/new'
+
+  get 'publishers/create'
+
+  get 'publishers/update'
+
+  get 'publishers/edit'
+
+  get 'publishers/destroy'
+
+  get 'publishers/index'
+
+  get 'publishers/show'
+
+  get 'authors/new'
+
+  get 'authors/create'
+
+  get 'authors/update'
+
+  get 'authors/edit'
+
+  get 'authors/destroy'
+
+  get 'authors/index'
+
+  get 'authors/show'
+
+  get 'categories/new'
+
+  get 'categories/create'
+
+  get 'categories/update'
+
+  get 'categories/edit'
+
+  get 'categories/destroy'
+
+  get 'categories/index'
+
+  get 'categories/show'
+
   get 'books/new'
 
   get 'books/create'
